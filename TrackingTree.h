3231da6fb2b6d303d80e64ef2c6480ee1f6edea7
JonPatrick99 <43153901+JonPatrick99@users.noreply.github.com>
2019-05-10 22:22:10 -0600

    Add files via upload

---
+++ TrackingTree.h
@@ -0,0 +1,32 @@
+// TrackingTree.h
+// Jon Caldwell
+// Project 2: Tracking Tree
+
+
+#include "TTnode.h"
+
+
+class TrackingTree
+{
+public:
+
+	TrackingTree();
+
+	TTnode* getTree();
+
+	void setTree(TTnode* in);								//sets tree variable
+
+	int insertNode(TTnode* cur, string in);					//finds where to insert a new node then inserts it
+	bool isFull(TTnode* in);								//tests if a tree is a full tree
+	int height(TTnode* in);								//returns the height of a tree
+	void changeCrawl(int height);							//crawls through entire tree updating hash values
+	vector<TTnode*> nodesAtH(TTnode* curTree, int height);	//returns a vector of nodes at height h for the changeCrawl function
+	void showNode(TTnode* inTree, string id);				//searches for a node then prints it
+	void changeNode(TTnode* inTree, string id, string raw);	//searches for a node then changes raw
+
+
+private:
+
+	TTnode* tree;
+
+};
