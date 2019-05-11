24358c50c31a00ff64fb59e553732c28192ed228
JonPatrick99 <43153901+JonPatrick99@users.noreply.github.com>
2019-05-10 22:34:14 -0600

    Update TrackingTree.h

--- TrackingTree.h
+++ TrackingTree.h
@@ -14,15 +14,14 @@ public:
 
 	TTnode* getTree();
 
-	void setTree(TTnode* in);								//sets tree variable
-
-	int insertNode(TTnode* cur, string in);					//finds where to insert a new node then inserts it
-	bool isFull(TTnode* in);								//tests if a tree is a full tree
-	int height(TTnode* in);								//returns the height of a tree
-	void changeCrawl(int height);							//crawls through entire tree updating hash values
-	vector<TTnode*> nodesAtH(TTnode* curTree, int height);	//returns a vector of nodes at height h for the changeCrawl function
-	void showNode(TTnode* inTree, string id);				//searches for a node then prints it
-	void changeNode(TTnode* inTree, string id, string raw);	//searches for a node then changes raw
+	void setTree(TTnode* in);								
+	int insertNode(TTnode* cur, string in);					
+	bool isFull(TTnode* in);								
+	int height(TTnode* in);								
+	void changeCrawl(int height);							
+	vector<TTnode*> nodesAtH(TTnode* curTree, int height);	
+	void showNode(TTnode* inTree, string id);				
+	void changeNode(TTnode* inTree, string id, string raw);	
 
 
 private:
